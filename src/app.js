dbc565994a6e1dffe7b8434891b2150551b763ab
ridho <ridhopratamavd@gmail.com>
2023-02-28 04:09:24 +0700

    add morgan logging

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const app = express();
 const tus = require('tus-node-server');
 const server = new tus.Server({ path: '/files' });
 server.datastore = new tus.FileStore({ directory: './files' });
+var morgan = require('morgan')
+morgan.token('id', function getId (req) {
+    return req.id
+})
+
+app.use(morgan('method::method url::url status::status content-length::res[content-length] - res-time::response-time ms'))
 
 app.all('/files/*', server.handle.bind(server));
 
