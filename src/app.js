b12120b47afc4cf8b6f654b2d0f99e8a3a2da37c
ridho <ridhopratamavd@gmail.com>
2023-02-28 22:13:17 +0700

    remove unnecessary interruption for every HTTP method

--- src/app.js
+++ src/app.js
@@ -16,31 +16,7 @@ server.datastore = new FileStore({directory: './files'})
 var morgan = require('morgan')
 app.use(morgan('method::method url::url status::status content-length::res[content-length] - res-time::response-time ms'))
 
-app.get('/files/*', (req, res, next) => {
-    console.log('1')
-});
-
-app.post('/files/*', (req, res, next) => {
-    console.log('2 :')
-    server.handle(req, res)
-});
-
-app.patch('/files/*', (req, res, next) => {
-    console.log('3 :')
-    server.handle(req, res)
-});
-
-app.put('/files/*', (req, res, next) => {
-    console.log('4')
-    server.handle(req, res)
-});
-
-app.get('/', (req, res) => {
-    console.log('5')
-    res.send('Hello World!');
-});
-
-// app.all("/files/*", server.handle.bind(server));
+app.all("/files/*", server.handle.bind(server));
 
 app.listen(8080, () => {
     console.log('Server listening on port 3000');
