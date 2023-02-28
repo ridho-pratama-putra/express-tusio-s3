07be7e25f8bfd13a14a13334005a3febac47df84
ridho <ridhopratamavd@gmail.com>
2023-02-28 18:31:09 +0700

    catch header submitted from client. required to update filename  saved from 'hashed filename' to become its original name

--- src/app.js
+++ src/app.js
@@ -8,13 +8,31 @@ morgan.token('id', function getId (req) {
     return req.id
 })
 
-app.use(morgan('method::method url::url status::status content-length::res[content-length] - res-time::response-time ms'))
+app.use(morgan('method::method url::url header::req[headers] resHeader::res[header] status::status content-length::res[content-length] - res-time::response-time ms'))
+app.get('/files/*', (req, res, next) => {
+    console.log('1')
+});
 
-app.all('/files/*', server.handle.bind(server));
+app.post('/files/*', (req, res, next) => {
+    console.log('2 : ', req.headers)
+    next();
+});
+
+app.patch('/files/*', (req, res, next) => {
+    console.log('3 :', req.headers)
+    next();
+});
+
+app.put('/files/*', (req, res, next) => {
+    console.log('4')
+});
 
 app.get('/', (req, res) => {
+    console.log('5')
     res.send('Hello World!');
 });
+
+app.all("/files/*", server.handle.bind(server));
 
 app.listen(8080, () => {
     console.log('Server listening on port 3000');
