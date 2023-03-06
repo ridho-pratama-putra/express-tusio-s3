1cd6c5ece72c1c8513638754d6f08e24aad033ab
ridho <ridhopratamavd@gmail.com>
2023-03-06 19:02:53 +0700

    add cors & handle each /files/* manually to log process

--- src/app.js
+++ src/app.js
@@ -15,8 +15,38 @@ server.datastore = new FileStore({directory: './files'})
 
 var morgan = require('morgan')
 app.use(morgan('method::method url::url status::status content-length::res[content-length] - res-time::response-time ms'))
+const cors = require('cors');
+app.use(cors())
 
-app.all("/files/*", server.handle.bind(server));
+app.get('/files/*', (req, res, next) => {
+    console.log('1')
+    server.handle(req, res)
+});
+
+app.post('/files/*', (req, res, next) => {
+    console.log('2 :')
+    server.handle(req, res)
+});
+
+app.patch('/files/*', (req, res, next) => {
+    console.log('3 :')
+    server.handle(req, res)
+});
+app.put('/files/*', (req, res, next) => {
+    console.log('4')
+    server.handle(req, res)
+});
+
+app.get('/', (req, res) => {
+    console.log('5')
+    res.send('Hello World!');
+});
+
+// handle when frontend use abort(true) instead of abort()
+app.delete('/files/*', (req, res, next) => {
+    console.log('6 :')
+    server.handle(req, res)
+});
 
 const port = 8080;
 app.listen(port, () => {
